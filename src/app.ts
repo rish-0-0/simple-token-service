ea0e95447cf1619f10691278307dcb3aef31e4cf
Rishabh Anand <rishabhanand42@gmail.com>
2021-09-26 15:41:22 +0530

    add fastify-swagger

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,24 @@ import generateRoutes from './routes/generate.router';
 import refreshRoutes from './routes/refresh.router';
 import verifyRoutes from './routes/verify.router';
 import errorHandler from './utils/error-handler';
+import swagger from 'fastify-swagger';
+import packageJSON from '../package.json';
 
 async function app () {
   const instance: Fastify.FastifyInstance = Fastify.fastify();
   instance.setErrorHandler(errorHandler);
+  instance.register(swagger, {
+    routePrefix: '/',
+    swagger: {
+      info: {
+        title: 'Simple Token Service',
+        description: 'Token Generate Service with Google Auth',
+        version: packageJSON.version
+      },
+      consumes: ['application/json'],
+      produces: ['application/json']
+    }
+  });
   instance.addHook('preValidation', googleAuthVerify);
   instance.register(generateRoutes);
   instance.register(verifyRoutes);
